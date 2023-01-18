829e565eaca00b48468a8092e4f21259f314c40a
Stiflerzak <hilarymwendia97@gmail.com>
2023-01-18 14:26:55 +0300

    Added features

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import blogData from '../data/blog'
+
+function Article({id,title,date="January 1, 1970",preview}) {
+  return (
+    <div>
+    <article>
+    <h3>{title}</h3>
+    <small>{date}</small>
+    <p>{preview}</p>
+    </article>
+    </div>
+  )
+}
+export default Article
